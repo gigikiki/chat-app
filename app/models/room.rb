0765ab718f67bc053f22ea5e694b8d12dde695be
gigikiki <75074619+gigikiki@users.noreply.github.com>
2020-12-11 16:38:26 +0900

    チャットルーム作成機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,11 +1,13 @@
 class Room < ApplicationRecord
-  
+ 
   def change
     create_table :rooms do |t|
       t.string :name, null: false
       t.timestamps
     end
   end
+  validates :name, presence: true
+  
   has_many :room_users
   has_many :users, through: :room_users
 
